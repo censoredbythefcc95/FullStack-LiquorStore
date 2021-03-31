9ae0c2afc6b56850ee9c4244dfa42ebeb087cade
Steven <sastudillo6@gmail.com>
2021-03-31 02:09:24 -0400

    Created Navigation Bar and Header Footer components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+    <Header />
+    <main className="py-3">
+      <Container>
       <h1> Welcome to BOOZEBROS. Coming Soon. </h1>
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }
