a34ea1a70790c44818068a962a02e0f6faef4526
Steven <sastudillo6@gmail.com>
2021-04-05 01:05:15 -0400

    Created middleware for user authentication based on JWT web token. Encryption successful.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ import generateToken from '../utils/generateToken.js';
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
 
-    const user = await User.findOne({ email: email })
+    const user = await User.findOne({ email })
 
     if(user && (await user.matchPassword(password))) {
         res.json({
@@ -32,12 +32,12 @@ const authUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
-    if (user) {
+    if(user) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
-            isAdmin: user.isAdmin,  
+            isAdmin: user.isAdmin,
         })
     } else {
         res.status(404)
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,28 +1,27 @@
-import jwt from 'jsonwebtoken';
-import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
 
-const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async(req, res, next) => {
     let token
 
-    if(req.headers.authorization && 
-       req.headers.authorization.startsWith('Bearer')
-     ) { 
-         try {
-             token = req.headers.authorization.split(' ')[1]
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')
+    ) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
 
-             const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-             res.user = await User.findById(decoded.id).select('-password')
+            req.user = await User.findById(decoded.id).select('-password')
 
-             next()
-
-         } catch (error) {
+            next()
+        } catch (error) {
             console.error(error)
             res.status(401)
             throw new Error('Not authorized, token failed.')
-         }
+        }
     }
+
 
     if(!token) {
         res.status(401)
