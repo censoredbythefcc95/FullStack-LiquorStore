df1abbf16bcc47ca0cf85d6145797a913170a009
Steven <sastudillo6@gmail.com>
2021-04-17 22:38:28 -0400

    Admin can now assign admin to other users and edit.

--- frontend/src/screens/UserEditScreen.js
+++ frontend/src/screens/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUser } from '../actions/userActions'
+import { USER_UPDATE_RESET } from '../constants/userConstants'
 
 const UserEditScreen = ({ match, history }) => {
     const userId = match.params.id
@@ -19,7 +20,15 @@ const UserEditScreen = ({ match, history }) => {
     const userDetails = useSelector((state) => state.userDetails)
     const { loading, error, user } = userDetails
 
+    const userUpdate = useSelector((state) => state.userUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate
+
+
     useEffect(() => {
+        if(successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET })
+            history.push('/admin/userlist')
+        } else {
         if (!user.name || user._id !== userId) {
             dispatch(getUserDetails(userId))
           } else {
@@ -27,10 +36,12 @@ const UserEditScreen = ({ match, history }) => {
             setEmail(user.email)
             setIsAdmin(user.isAdmin)
           }
-    }, [dispatch, history, userId, user])
+        }
+    }, [dispatch, history, userId, user, successUpdate])
 
         const submitHandler = (e) => {
             e.preventDefault()
+            dispatch(updateUser({ _id: userId, name, email, isAdmin }))
         }
 
     return (
@@ -40,6 +51,8 @@ const UserEditScreen = ({ match, history }) => {
             </Link>
             <FormContainer>
             <h1>Edit User</h1>
+            {loadingUpdate && <Loader />}
+            {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <Form onSubmit={submitHandler}>
                 <Form.Group controlId='name'>
