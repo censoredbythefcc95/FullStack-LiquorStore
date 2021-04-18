7ea439f7ccfce733fc3531bf7ffb60fdbda28e9c
Steven <sastudillo6@gmail.com>
2021-04-17 23:28:56 -0400

    Added delete endpoint for products. Products showing on admin screen, and admin can now delete products from database with the middleware.

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,10 @@
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL
 } from '../constants/productConstants';
 import {
     PRODUCT_DETAILS_REQUEST,
@@ -29,6 +32,19 @@ export const productDetailsReducer = (state = { product: { reviews: [] } }, acti
         case PRODUCT_DETAILS_SUCCESS:
             return { loading: false, product: action.payload }
         case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+        default: 
+            return state
+    }
+}
+
+export const productDeleteReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_DELETE_REQUEST:
+            return { loading: true }
+        case PRODUCT_DELETE_SUCCESS:
+            return { loading: false, success: true }
+        case PRODUCT_DELETE_FAIL:
             return { loading: false, error: action.payload }
         default: 
             return state
