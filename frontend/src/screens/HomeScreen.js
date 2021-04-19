974e44214c60439e7ea02694b3a875a63f334489
Steven <sastudillo6@gmail.com>
2021-04-19 06:09:24 -0400

    App is complete at this time. Preparing for Heroku deployment.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import Product from "../components/Product"
 import { Row, Col } from "react-bootstrap";
@@ -7,6 +8,7 @@ import Loader from '../components/Loader';
 import Paginate from '../components/Paginate'
 import ProductCarousel from '../components/ProductCarousel'
 import { listProducts } from '../actions/productActions';
+import Meta from '../components/Meta'
 
 const HomeScreen = ({ match }) => {
     const keyword = match.params.keyword
@@ -23,7 +25,8 @@ const HomeScreen = ({ match }) => {
 
     return (
         <>
-        {!keyword && <ProductCarousel />}
+        <Meta />
+        {!keyword ? <ProductCarousel /> : <Link to='/' className='btn btn-light'>Go Back</Link>}
             <h1>Latest Drinks</h1>
             {loading ? (
                  <Loader />
