e09d485952cb59603f693aa43a6180803a75bad9
Steven <sastudillo6@gmail.com>
2021-04-18 23:36:44 -0400

    Admin can now add images and new products to the database, and the store will update with this product on the frontpage.

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
-import { listProductDetails  } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
 
 const ProductEditScreen = ({ match, history }) => {
     const productId = match.params.id
@@ -23,7 +24,15 @@ const ProductEditScreen = ({ match, history }) => {
     const productDetails = useSelector((state) => state.productDetails)
     const { loading, error, product } = productDetails
 
+
+    const productUpdate = useSelector((state) => state.productUpdate)
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate
+
     useEffect(() => {
+        if(successUpdate) {
+            dispatch({ type: PRODUCT_UPDATE_RESET })
+            history.push('/admin/productlist')
+        } else {
         if (!product.name || product._id !== productId) {
             dispatch(listProductDetails(productId))
           } else {
@@ -36,11 +45,19 @@ const ProductEditScreen = ({ match, history }) => {
             setDescription(product.description)
           }
         }
-, [dispatch, history, productId, product])
+    }
+, [dispatch, history, productId, product, successUpdate])
 
         const submitHandler = (e) => {
             e.preventDefault()
-            // UPDATE PRODUCT
+            dispatch(updateProduct({
+                _id: productId,
+                name,
+                price,
+                image,
+                brand, category, description,
+                countInStock
+            }))
         }
 
     return (
@@ -50,6 +67,8 @@ const ProductEditScreen = ({ match, history }) => {
             </Link>
             <FormContainer>
             <h1>Edit Product</h1>
+            {loadingUpdate && <Loader />}
+            {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
                 <Form onSubmit={submitHandler}>
                 <Form.Group controlId='name'>
@@ -87,15 +106,6 @@ const ProductEditScreen = ({ match, history }) => {
                         onChange={(e) => setBrand(e.target.value)}>
                         </Form.Control>
                     </Form.Group>
-
-                    <Form.Group controlId='price'>
-                        <Form.Label>
-                            Price
-                        </Form.Label>
-                        <Form.Control type='number' placeholder='Enter price' value={price}
-                        onChange={(e) => setPrice(e.target.value)}>
-                        </Form.Control>
-                        </Form.Group>
 
                         <Form.Group controlId='countInStock'>
                         <Form.Label>
