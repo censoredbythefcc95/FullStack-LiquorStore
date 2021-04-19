980de25b550c1f3459888d7c39df94a8e94198ab
Steven <sastudillo6@gmail.com>
2021-04-19 00:49:16 -0400

    Admin can now upload photos into upload folder via the multer middleware.

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect } from 'react'
+import axios from 'axios'
 import { Form, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ match, history }) => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const dispatch = useDispatch()
 
@@ -47,6 +49,29 @@ const ProductEditScreen = ({ match, history }) => {
         }
     }
 , [dispatch, history, productId, product, successUpdate])
+
+        const uploadFileHandler =  async (e) => {
+            const file = e.target.files[0]
+            const formData = new FormData()
+            formData.append('image', file)
+            setUploading(true)
+
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+
+                const { data } = await axios.post('/api/upload', formData, config)
+
+                setImage(data)
+                setUploading(false)
+            } catch (error) {
+                console.error(error)
+                setUploading(false)
+            }
+        }
 
         const submitHandler = (e) => {
             e.preventDefault()
@@ -96,6 +121,11 @@ const ProductEditScreen = ({ match, history }) => {
                         <Form.Control type='text' placeholder='Enter image URL' value={image}
                         onChange={(e) => setImage(e.target.value)}>
                         </Form.Control>
+                        <Form.File id='image-file' label='Choose file'
+                        custom onChange={uploadFileHandler}>
+
+                        </Form.File>
+                        {uploading && <Loader />}
                     </Form.Group>
     
                     <Form.Group controlId='brand'>
@@ -105,6 +135,7 @@ const ProductEditScreen = ({ match, history }) => {
                         <Form.Control type='text' placeholder='Enter brand name' value={brand}
                         onChange={(e) => setBrand(e.target.value)}>
                         </Form.Control>
+                        
                     </Form.Group>
 
                         <Form.Group controlId='countInStock'>
